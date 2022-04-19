6e0817154a0952e8544bc5d9cb0e3af521798a93
adadesions <adadesions@gmail.com>
2022-04-20 03:18:00 +0700

    Add layout

---
+++ 100days-generator/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return (
+      <div className="container mx-auto bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 my-4 py-4 px-4">
+        <h1 className="text-yellow-300 hover:text-yellow-100 text-2xl font-black text-center">
+            powered by Adabrain.tv
+        </h1>
+      </div>
+    );
+  };
+  
+  export default Footer;
+  
